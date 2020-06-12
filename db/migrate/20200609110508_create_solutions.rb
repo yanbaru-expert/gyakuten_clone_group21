abbd090c247777a0fea4bdb5445ae680cf4b900d
柴谷遼太朗 <shibataniryotaro@shibataniryoutarounoMacBook-Air.local>
2020-06-12 20:44:27 +0900

    defined association

--- db/migrate/20200609110508_create_solutions.rb
+++ db/migrate/20200609110508_create_solutions.rb
@@ -1,8 +1,9 @@
 class CreateSolutions < ActiveRecord::Migration[6.0]
   def change
     create_table :solutions do |t|
-      t.text :solution
-    
+      t.text :answer
+      t.references :question, foreign_key: true
+
       t.timestamps
     end
   end
